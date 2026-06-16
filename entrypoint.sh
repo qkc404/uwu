4ed534070a9e4519898fdb695d03d32b646acd86
404 <141468084+qkc404@users.noreply.github.com>
2026-06-16 21:10:42 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "[$(date)] Starting Xray..."
 /usr/local/bin/xray run -c /etc/xray/config.json 2>&1 &
 
 echo "[$(date)] Waiting for Xray to initialize..."
-sleep 5
+sleep 15
 
 echo "[$(date)] Starting Nginx..."
 exec /usr/local/openresty/bin/openresty -g 'daemon off;'
